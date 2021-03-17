97b2cedb23de0a8006c6b16f7b105efaecaafa6b
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-03-17 17:20:00 +0900

    update gatsby-node.jsand query

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,14 @@
-exports.onCreatenode = ({node, actions}) => {
+const {createFilePath} = require("gatsby-source-filesystem");
+
+exports.onCreateNode = ({node, getNode ,actions}) => {
     const { createNodeField } = actions 
-    if(node.internal.type === "MarkdownRemark"){
-        
+    if(node.internal.type === 'MarkdownRemark'){
+        const slug = createFilePath({node, getNode});
+        createNodeField({
+            node,
+            name:`slug`,
+            value:slug
+        })
     }
 }
+
